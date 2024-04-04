36b920b4855b516efb3ac8209c6307afc40b2f58
ELthomasoPostfix <thomas.gueutal@student.uantwerpen.be>
2024-04-04 22:00:23 +0200

    Refactor day3  predicate.
    The predicate now groups the Xstart, Xend, Y
     params into a single tuple instead of accepting
     them as separate paremeters. This makes passing
     them in much cleaner.

--- Prolog/day3.pl
+++ Prolog/day3.pl
@@ -115,13 +115,13 @@ marked_sum(Sum) :-
 
 % Find numbers neighboured by any mark at all.
 marked_num(Number) :-
-    num_coordinates(Number, (Xstart, Xend, Y)),
-    contains_mark(_, Xstart, Xend, Y).
+    num_coordinates(Number, NumCoords),
+    contains_mark(_, NumCoords).
 
 
 % Check if the rectangle defined by the points (Xstart-1, Y-1) and (Xend+1, Y+1)
 % contains any marking character.
-contains_mark(Mark, Xstart, Xend, Y) :-
+contains_mark(Mark, (Xstart, Xend, Y)) :-
     mark_coordinates(Mark, (Xmark, Ymark)),
     (eval(Xstart - 1) =< Xmark), (Xmark =< eval(Xend + 1)),
     (eval(Y - 1)      =< Ymark), (Ymark =< eval(Y + 1)),
